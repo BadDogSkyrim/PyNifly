6258d7c5117d2f6617f733e5734cf8cb5a5991f3
BadDogSkyrim <apatrushko@yahoo.com>
2021-08-09 14:19:10 -0400

    Bumping version number

--- PyNifly/__init__.py
+++ PyNifly/__init__.py
@@ -11,7 +11,7 @@ bl_info = {
     "description": "Nifly Import/Export for Skyrim, Skyrim SE, and Fallout 4 NIF files (*.nif)",
     "author": "Bad Dog",
     "blender": (2, 92, 0),
-    "version": (0, 0, 46),  
+    "version": (0, 0, 47),  
     "location": "File > Import-Export",
     "warning": "WIP",
     "support": "COMMUNITY",
