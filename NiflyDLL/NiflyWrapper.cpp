f8b9ca13fc3ef3837e306b55125e561e4fc4fa2f
BadDogSkyrim <apatrushko@yahoo.com>
2022-04-09 08:16:46 -0400

    Version bump

--- NiflyDLL/NiflyWrapper.cpp
+++ NiflyDLL/NiflyWrapper.cpp
@@ -21,7 +21,7 @@
 #include "NiflyFunctions.hpp"
 #include "NiflyWrapper.hpp"
 
-const int NiflyDDLVersion[3] = { 4, 1, 14 };
+const int NiflyDDLVersion[3] = { 4, 2, 0 };
  
 using namespace nifly;
 
