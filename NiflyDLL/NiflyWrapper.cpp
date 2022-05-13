0895fccff020abb0417c13abb46806a3ff0218ed
BadDogSkyrim <apatrushko@yahoo.com>
2022-05-12 22:21:01 -0400

    Moved skyrim vampire morph to the chargen tri

--- NiflyDLL/NiflyWrapper.cpp
+++ NiflyDLL/NiflyWrapper.cpp
@@ -21,7 +21,7 @@
 #include "NiflyFunctions.hpp"
 #include "NiflyWrapper.hpp"
 
-const int NiflyDDLVersion[3] = { 5, 2, 0 };  
+const int NiflyDDLVersion[3] = { 5, 3, 0 };  
  
 using namespace nifly;
 
