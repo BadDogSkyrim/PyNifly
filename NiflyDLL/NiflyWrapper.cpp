423a3eed209d920bb5b47f1daf704dc7caaaf875
Bad Dog <apatrushko@yahoo.com>
2022-11-18 07:54:31 -0500

    Shortened the facebones bone appearance to make them more convenient to use.

--- NiflyDLL/NiflyWrapper.cpp
+++ NiflyDLL/NiflyWrapper.cpp
@@ -21,7 +21,7 @@
 #include "NiflyFunctions.hpp"
 #include "NiflyWrapper.hpp"
 
-const int NiflyDDLVersion[3] = { 6, 1, 0 }; 
+const int NiflyDDLVersion[3] = { 6, 2, 0 }; 
  
 using namespace nifly;
 
