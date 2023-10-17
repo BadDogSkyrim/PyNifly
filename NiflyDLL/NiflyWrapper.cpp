131c204f56f12c514cabe0a2aca1521bd1c7fab6
Bad Dog <apatrushko@yahoo.com>
2023-10-17 14:16:12 -0400

    Version bump.

--- NiflyDLL/NiflyWrapper.cpp
+++ NiflyDLL/NiflyWrapper.cpp
@@ -19,7 +19,7 @@
 #include "NiflyFunctions.hpp"
 #include "NiflyWrapper.hpp"
 
-const int NiflyDDLVersion[3] = { 12, 2, 0 }; 
+const int NiflyDDLVersion[3] = { 12, 3, 0 }; 
  
 using namespace nifly;
 
