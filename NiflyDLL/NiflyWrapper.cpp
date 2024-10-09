1df63b8398ea6141e6d0e772ba9b8bb2b9ebb844
Bad Dog <apatrushko@yahoo.com>
2024-10-09 13:31:37 -0400

    Version bump.

--- NiflyDLL/NiflyWrapper.cpp
+++ NiflyDLL/NiflyWrapper.cpp
@@ -19,7 +19,7 @@
 #include "NiflyFunctions.hpp"
 #include "NiflyWrapper.hpp"
 
-const int NiflyDDLVersion[3] = { 18, 2, 0 };
+const int NiflyDDLVersion[3] = { 18, 3, 0 };
  
 using namespace nifly;
 
--- PyNifly/__init__.py
+++ PyNifly/__init__.py
@@ -7,7 +7,7 @@ bl_info = {
     "description": "Nifly Import/Export for Skyrim, Skyrim SE, and Fallout 4 NIF files (*.nif)",
     "author": "Bad Dog",
     "blender": (4, 0, 0),
-    "version": (18, 2, 0),   
+    "version": (18, 3, 0),   
     "location": "File > Import-Export",
     "support": "COMMUNITY",
     "category": "Import-Export"
