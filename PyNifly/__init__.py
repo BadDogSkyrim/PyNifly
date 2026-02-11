a123399d8106564f7c942132bc345dc6318bcec8
Bad Dog <apatrushko@yahoo.com>
2026-02-11 10:45:27 -0500

    Changing when preferences are defined.

--- PyNifly/__init__.py
+++ PyNifly/__init__.py
@@ -7,7 +7,7 @@ bl_info = {
     "description": "Nifly Import/Export for Skyrim, Skyrim SE, and Fallout 4 NIF files (*.nif)",
     "author": "Bad Dog",
     "blender": (4, 5, 0),
-    "version": (23, 0, 1),   
+    "version": (23, 0, 2),   
     "location": "File > Import-Export",
     "support": "COMMUNITY",
     "category": "Import-Export"
@@ -17,12 +17,6 @@ from contextlib import suppress
 import bpy
 from bpy.types import AddonPreferences
 from bpy.props import StringProperty, BoolProperty
-from .util.settings import ImportSettings, ExportSettings
-
-from . import nif
-from . import tri
-from . import hkx
-from . import kf
 
 class PyNiflyPreferences(AddonPreferences):
     bl_idname = __package__   # critical: must match your add-on module name
@@ -79,14 +73,14 @@ class PyNiflyPreferences(AddonPreferences):
         name="Blender-friendly bone names",
         description=("Renames bones according to Blender conventions, e.g. .L for left and" + 
                      " .R for right. Disable if you need to match the game's bone names."),
-        default=ImportSettings.__dataclass_fields__["rename_bones"].default,
+        default=True,
     ) # type: ignore
 
     rename_bones_niftools: BoolProperty(
         name="NifTools-friendly bone names",
         description=("Renames bones to match the NifTools importer/exporter. Use if you " +
                      "need interoperability with NifToos."),
-        default=ImportSettings.__dataclass_fields__["rename_bones_niftools"].default
+        default=False
     ) # type: ignore
 
     rotate_bones_pretty: BoolProperty(
@@ -94,21 +88,21 @@ class PyNiflyPreferences(AddonPreferences):
         description=("Bone orientation matches limbs in humanoid skeletons. " +
                      "Should have no other effect. Disable if you see problems."),
         options={'HIDDEN'},
-        default=ImportSettings.__dataclass_fields__["rotate_bones_pretty"].default,
+        default=False
     ) # type: ignore
 
     import_tris: BoolProperty(
         name="Import tri files when found",
         description=("If importing a nif and a tri file is found with the same name, or the "
             + "name + 'chargen', import the tri as well."),
-        default=ImportSettings.__dataclass_fields__["import_tris"].default,
+        default=True
     ) # type: ignore
 
     blender_xf: BoolProperty(
         name="Blender-friendly scene orientation",
         description=("Rotates the scene 90 degrees around the Z axis and scale down to match Blender. " +
                      "Disable to preserve the original orientation."),
-        default=ImportSettings.__dataclass_fields__["blender_xf"].default
+        default=False
     ) # type: ignore
 
 
@@ -130,15 +124,27 @@ class PyNiflyPreferences(AddonPreferences):
 
 def register():
     bpy.utils.register_class(PyNiflyPreferences)
+
+    from . import nif
+    from . import tri
+    from . import hkx
+    from . import kf
+
     hkx.register()
     kf.register()
     nif.register()
     tri.register()
 
 def unregister():
-    with suppress(RuntimeError):
-        bpy.utils.unregister_class(PyNiflyPreferences)
+    from . import nif
+    from . import tri
+    from . import hkx
+    from . import kf
+
     hkx.unregister()
     kf.unregister()
     nif.unregister()
     tri.unregister()
+
+    with suppress(RuntimeError):
+        bpy.utils.unregister_class(PyNiflyPreferences)
