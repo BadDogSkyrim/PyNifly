06387a3745a7bd93dbcd3668343b4197deb9ee41
Bad Dog <apatrushko@yahoo.com>
2026-02-12 12:37:35 -0500

    Version bump.

--- io_scene_nifly/__init__.py
+++ io_scene_nifly/__init__.py
@@ -7,7 +7,7 @@ bl_info = {
     "description": "Nifly Import/Export for Skyrim, Skyrim SE, and Fallout 4 NIF files (*.nif)",
     "author": "Bad Dog",
     "blender": (4, 0, 0),
-    "version": (23, 0, 7),   
+    "version": (23, 1, 0),   
     "location": "File > Import-Export",
     "support": "COMMUNITY",
     "category": "Import-Export"
@@ -144,7 +144,7 @@ def register():
 
 def unregister():
     if not BLENDER_AVAILABLE: return
-    
+
     from . import nif
     from . import tri
     from . import hkx
